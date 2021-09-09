8b9d49d2ab9c31ae2dbfee2b55766f919882e792
Tuan N. Truong <tuan.truong625@gmail.com>
2021-09-09 16:13:23 -0400

    Setup websocket for tickers

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import protobuf from 'protobufjs'
+const { Buffer } = require('buffer/')
+// import Buffer from 'buffer'
 
 function App() {
+  const [current, setCurrent] = useState < protobuf.Message < {} > | undefined > ()
+
+  useEffect(() => {
+    const ws = new WebSocket('wss://streamer.finance.yahoo.com');
+    protobuf.load('./YPricingData.proto', (error, root) => {
+      if (error) {
+        console.log(error);
+      }
+      const Yaticker = root?.lookupType('yaticker');
+
+      ws.onopen = function open() {
+        console.log('connected');
+        ws.send(JSON.stringify({
+          subscribe: ['AMC']
+        }));
+      };
+
+      ws.onclose = function close() {
+        console.log('disconnected');
+      };
+
+      ws.onmessage = function incoming(message) {
+        const next: protobuf.Message<{}> | undefined = Yaticker?.decode(new Buffer(message.data, 'base64'))
+        setCurrent(next)
+      };
+    })
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>STONKS</h1>
+      {
+        // current ?? <h2>{current?.id}</h2>
+      }
     </div>
   );
 }

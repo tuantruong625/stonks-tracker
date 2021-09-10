2d3cba284441ef7ad54061adf30d17cb4f3f8373
Tuan N. Truong <tuan.truong625@gmail.com>
2021-09-10 09:51:07 -0400

    Render red or green on ticker

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ function App() {
     <div className="mx-auto mt-10 max-w-4x flex flex-col justify-center items-center h-screen">
       <div className="flex flex-col justify-center  border border-gray-100 shadow-md rounded-md p-4">
         <h1 className="text-gray-600 text-xl font-medium">{symbol.id }</h1>
-        <p className="text-3xl pr-2 text-gray-700">${ symbol.price.toFixed(2) }</p>
+        <p className="text-3xl pr-2 text-gray-700">${ symbol.price }</p>
         <div className="flex ">
-          <p className={'text-green-500'}>{ symbol.change.toFixed(2) }</p>
-          <p className={'text-green-500'}>({ symbol.changePercent.toFixed(2) }%)</p>
+          <p className={ Math.sign(symbol.change) ? 'text-green-500' : 'text-red-500'}>{ symbol.change }</p>
+          <p className={ Math.sign(symbol.changePercent) ? 'text-green-500' : 'text-red-500'}>({ symbol.changePercent }%)</p>
         </div>
         <p className="text-gray-500">{symbol.time}</p>
       </div>

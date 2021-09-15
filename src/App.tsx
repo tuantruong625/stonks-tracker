7e66a2c2832dde74a90950d59b82ab8153a0547e
Tuan N. Truong <tuan.truong625@gmail.com>
2021-09-15 15:45:43 -0400

    Format date and time

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
 import useStonkFeed from './hooks';
+import { DateTime } from 'luxon'
 
 function App() {
   const symbol = useStonkFeed()
 
+  const payload = {
+    "id": "AMC",
+    "price": 46.970001220703125,
+    "time": "1631733631000",
+    "exchange": "NYQ",
+    "quoteType": "EQUITY",
+    "marketHours": "REGULAR_MARKET",
+    "changePercent": -0.6976702213287354,
+    "dayVolume": "71008670",
+    "change": -0.3299980163574219,
+    "priceHint": "2"
+  }
+
   function toFixed(num: number, fixed: number) {
     var re = new RegExp('^-?\\d+(?:\.\\d{0,' + (fixed || -1) + '})?');
     return num?.toString().match(re)[0];
+  }
+
+  function timestampToTimeWithSeconds(timestamp: number) {
+    return DateTime.fromMillis(timestamp).toFormat('tt')
+  }
+
+  function timestampToDateWithMonthAndWeekday(timestamp: number) {
+    return DateTime.fromMillis(timestamp).toFormat('DDDD')
   }
 
   return (
@@ -19,7 +41,7 @@ function App() {
           <p className={ !Math.sign(symbol.change) ? 'text-green-500' : 'text-red-500'}>{ toFixed(symbol.change, 2) }</p>
           <p className={!Math.sign(symbol.changePercent) ? 'text-green-500' : 'text-red-500'}>({toFixed(symbol.changePercent,2) }%)</p>
         </div>
-        <p className="text-gray-500">{symbol.time}</p>
+        <p className="text-gray-500">{symbol.time && timestampToTimeWithSeconds(symbol.time) }</p>
       </div>
     </div>
   );
